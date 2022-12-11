6928c7f2f33b3985a72928b60a6e8114b0be7d21
Leonid Samoshenko <86494836+LeeDoor@users.noreply.github.com>
2022-12-11 12:31:33 +0300

    Update MainWindow.cpp

--- Overwatch 2 Ballancer/MainWindow.cpp
+++ Overwatch 2 Ballancer/MainWindow.cpp
@@ -1,27 +1,19 @@
 #include "MainWindow.h"
+#include <qrandom.h>
 
 MainWindow::MainWindow(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
 
-
+	QRandomGenerator gen;
 	QPixmap* map = new QPixmap(50, 50);
 	map->fill(QColor("red"));
-	ui.allPlayers->addPlayer(QString("bababa1"), map);
-	ui.allPlayers->addPlayer(QString("bababa2"), map);
-	ui.allPlayers->addPlayer(QString("bababa3"), map);
-	ui.allPlayers->addPlayer(QString("bababa4"), map);
-	ui.allPlayers->addPlayer(QString("bababa5"), map);
-	ui.allPlayers->addPlayer(QString("bababa6"), map);
-	ui.allPlayers->addPlayer(QString("pogiloypawuk#2415"), map);
-	ui.allPlayers->addPlayer(QString("bababa1"), map);
-	ui.allPlayers->addPlayer(QString("bababa2"), map);
-	ui.allPlayers->addPlayer(QString("bababa3"), map);
-
-	ui.activePlayers->addPlayer(QString("bababa1"), map);
-	ui.activePlayers->addPlayer(QString("bababa2"), map);
-	ui.activePlayers->addPlayer(QString("bababa3"), map);
+	for (int i = 0; i < 100; ++i) {
+		map->fill(QColor(gen.bounded(255), gen.bounded(255), gen.bounded(255)));
+		ui.allPlayers->addPlayer(QString("pogiloypawuk#2415"), map);
+	}
+	for(int i = 0;i < 50; ++i)ui.activePlayers->addPlayer(QString("pogiloypawuk#2415"), map);
 }
 
 MainWindow::~MainWindow()
