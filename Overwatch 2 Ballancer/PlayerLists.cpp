2cf03a8714520f1be7f921997e8d025d2eb1001f
Leonid Samoshenko <86494836+LeeDoor@users.noreply.github.com>
2022-12-15 15:20:53 +0300

    player lists class. not tested

--- Overwatch 2 Ballancer/PlayerLists.cpp
+++ Overwatch 2 Ballancer/PlayerLists.cpp
@@ -1,1 +1,57 @@
 #include "PlayerLists.h"
+#include <algorithm>
+
+bool PlayerLists::addPlayer(std::shared_ptr<Player> player) {
+	bool exists = (bool)hasPlayer(player);
+	if (!exists) {
+		allPlayers.push_back(player);
+	}
+	return exists;
+}
+
+bool PlayerLists::addActivePlayer(std::shared_ptr<Player> player) {
+	if (hasActivePlayer(player)) {
+		return false;
+	}
+	else if (hasPlayer(player)) {
+		actPlayers.push_back(player);
+	}
+}
+
+std::shared_ptr<Player> PlayerLists::hasPlayer(std::shared_ptr<Player> player) {
+	auto iter = std::find(allPlayers.begin(), allPlayers.end(), player);
+	if (iter == allPlayers.end()) return nullptr;
+	return *iter;
+}
+std::shared_ptr<Player> PlayerLists::hasActivePlayer(std::shared_ptr<Player> player) {
+	auto iter = std::find(actPlayers.begin(), actPlayers.end(), player);
+	if (iter == actPlayers.end()) return nullptr;
+	return iter->lock();
+}
+
+std::shared_ptr<Player> PlayerLists::editPlayer(std::string lastName, std::shared_ptr<Player> newPlayer) {
+	auto iter = std::find_if(allPlayers.begin(), allPlayers.end(), [&](std::shared_ptr<Player> p) {
+		p->identity.name == lastName; 
+	});
+	if (iter == allPlayers.end())return false;
+	*iter = newPlayer;
+	return *iter;
+}
+
+
+bool PlayerLists::removePlayer(std::shared_ptr<Player> player) {
+	auto iter = std::find(allPlayers.begin(), allPlayers.end(), player);
+	if (iter == allPlayers.end())return false;
+
+	std::remove(allPlayers.begin(), allPlayers.end(), player);
+	allPlayers.erase(iter);
+	iter->reset();
+	return true;
+}
+
+bool PlayerLists::removeActPlayer(std::shared_ptr<Player> player) {
+	auto iter = std::find(actPlayers.begin(), actPlayers.end(), player);
+	if (iter == actPlayers.end())return false;
+	actPlayers.erase(iter);
+	return true;
+}
--- Overwatch 2 Ballancer/PlayerLists.h
+++ Overwatch 2 Ballancer/PlayerLists.h
@@ -1,17 +1,37 @@
 #pragma once
-#include "Players.h"
+#include <vector>
+#include <memory>
+#include "Player.h"
 
 class PlayerLists
 {
 private:
-	static Players allPlayers;
-	static Players activePlayers;
+	static std::vector<std::shared_ptr<Player>> allPlayers;
+	static std::vector<std::weak_ptr<Player>> actPlayers;
 
 public:
-	static void addPlayer(std::shared_ptr<Player> player);
-	static void addActivePlayer(std::shared_ptr<Player> player);
-	static void hasPlayer(std::shared_ptr<Player> player);
-	static void hasActivePlayer(std::shared_ptr<Player> player);
-	static void editPlayer(std::string lastName, std::shared_ptr<Player> newPlayer);
+	//add player to given list. return true if player added
+	//return false if player already exists
+	static bool addPlayer(std::shared_ptr<Player> player);
+	static bool addActivePlayer(std::shared_ptr<Player> player);
+
+	//returns true if there is a given player in given list
+	static std::shared_ptr<Player> hasPlayer(std::shared_ptr<Player> player);
+	static std::shared_ptr<Player> hasActivePlayer(std::shared_ptr<Player> player);
+
+	//edits player with given name to given stats
+	//returns true if success, returns true if player not found
+	static std::shared_ptr<Player> editPlayer(std::string lastName, 
+								std::shared_ptr<Player> newPlayer);
+
+	//removes given player from global list. finds this player
+	//in actives, and if finds, removes too
+	//returns true if player found and removed, false overwise
+	static bool removePlayer(std::shared_ptr<Player> player);
+
+	//finds and removes active player. it remains in global list.
+	//returns true if player found and removed, false overwise
+	static bool removeActPlayer(std::shared_ptr<Player> player);
+
 };
 
